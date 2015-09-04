0fa510601655d18d9acdeb9ba4d9c9571e72cdad
yutaka-sano <yutaka.sano@dena.com>
2015-09-04 10:23:40 +0900

    add 05_Scenes

---
+++ 05_Scenes/swift-scenes/View2Controller.swift
@@ -0,0 +1,25 @@
+//
+//  View2Controller.swift
+//  swift-scenes
+//
+//  Created by Yutaka Sano on 2015/09/04.
+//  Copyright (c) 2015年 Yutaka Sano. All rights reserved.
+//
+
+import UIKit
+
+class View2Controller: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+

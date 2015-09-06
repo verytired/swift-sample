aa9d3fb205bfd12e3a33ced483be19c64bc5dece
verytired <sano.yutaka@gmail.com>
2015-09-06 23:52:22 +0900

    xcode 6.3対応

--- 08_Drawing/swift-drawing/drawView.swift
+++ 08_Drawing/swift-drawing/drawView.swift
@@ -11,33 +11,28 @@ import UIKit
 
 class drawView: UIView {
     
-    //6.
     var lines: [Line] = []
     var lastPoint: CGPoint!
     
-    
-    //4.
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         //self.backgroundColor = UIColor.blackColor() //チェック用
-        
     }
     
-    //7.
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        lastPoint = touches.anyObject()?.locationInView(self)
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        lastPoint = touch.locationInView(self)
     }
     
-    override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
-        var newPoint = touches.anyObject()?.locationInView(self)
-        lines.append(Line(start: lastPoint, end: newPoint!))
+    
+    override func touchesMoved(touches: Set<NSObject>, withEvent event: UIEvent) {
+        let touch = touches.first as! UITouch
+        let newPoint = touch.locationInView(self)
+        lines.append(Line(start: lastPoint, end: newPoint))
         lastPoint = newPoint
-        
         self.setNeedsDisplay()
-        
     }
     
-    //8.
     override func drawRect(rect: CGRect) {
         var context = UIGraphicsGetCurrentContext()
         CGContextBeginPath(context)

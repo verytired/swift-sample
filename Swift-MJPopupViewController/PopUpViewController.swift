cddf721b9f88a7fd2f901f059d2aa8a81f0060ad
verytired <sano.yutaka@gmail.com>
2015-09-08 22:06:53 +0900

    delete

--- Swift-MJPopupViewController/PopUpViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PopUpViewController.swift
-//  Swift-MJPopupViewController
-//
-//  Created by Yutaka Sano on 2015/09/08.
-//  Copyright (c) 2015年 Yutaka Sano. All rights reserved.
-//
-
-import Foundation

564045f5ff6d7b154ecad422afc15a12e78eab6d
yutaka-sano <yutaka.sano@dena.com>
2015-09-09 16:38:43 +0900

    modified 09

--- 09_PopUp/swift-popup/ViewController.swift
+++ 09_PopUp/swift-popup/ViewController.swift
@@ -20,7 +20,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
+
+    //通常
     @IBAction func onTappedAlert(sender: UIButton) {
         println("onPressButton")
         var alert = UIAlertView()
@@ -31,7 +32,39 @@ class ViewController: UIViewController {
 
     }
 
+    //通常
+    @IBAction func onTappedAlertController(sender: UIButton) {
+        let alertController = UIAlertController(title: "Hello!", message: "This is Alert sample.", preferredStyle: .Alert)
+        let otherAction = UIAlertAction(title: "OK", style: .Default) {
+            action in self.onPressOK()
+        }
+        
+        alertController.addAction(otherAction)
+        presentViewController(alertController, animated: true, completion: nil)
+        
+    }
     
+    func onPressOK(){
+        println("#onPressOK pushed OK!")
+    }
+    
+    //選択
+    @IBAction func choiceBtn(sender: UIButton) {
+        let alertController = UIAlertController(title: "Hello!", message: "This is Alert sample.", preferredStyle: .Alert)
+        let otherAction = UIAlertAction(title: "OK", style: .Default) {
+            action in println("pushed OK!")
+        }
+        let cancelAction = UIAlertAction(title: "CANCEL", style: .Cancel) {
+            action in println("Pushed CANCEL!")
+        }
+        
+        alertController.addAction(otherAction)
+        alertController.addAction(cancelAction)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+
+    //ボトム表示
     @IBAction func onTappedPopUp(sender: UIButton) {
         let alertController = UIAlertController(title: "Hello!", message: "This is Alert sample.", preferredStyle: .ActionSheet)
         let firstAction = UIAlertAction(title: "First", style: .Default) {
@@ -56,6 +89,7 @@ class ViewController: UIViewController {
         presentViewController(alertController, animated: true, completion: nil)
     }
     
+    //フォーム付
     @IBAction func inputFieldBtn(sender: UIButton) {
         var inputTextField: UITextField?
         var passwordField: UITextField?
@@ -87,19 +121,7 @@ class ViewController: UIViewController {
         presentViewController(alertController, animated: true, completion: nil)
     }
     
-    @IBAction func choiceBtn(sender: UIButton) {
-        let alertController = UIAlertController(title: "Hello!", message: "This is Alert sample.", preferredStyle: .Alert)
-        let otherAction = UIAlertAction(title: "OK", style: .Default) {
-            action in println("pushed OK!")
-        }
-        let cancelAction = UIAlertAction(title: "CANCEL", style: .Cancel) {
-            action in println("Pushed CANCEL!")
-        }
-        
-        alertController.addAction(otherAction)
-        alertController.addAction(cancelAction)
-        presentViewController(alertController, animated: true, completion: nil)
-    }
+
     
 }
 
